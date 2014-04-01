067140022d188036f807bd779f911ef64e6e6dd0
Tom Andrews <git@tomandrews.co.uk>
2014-04-01 13:30:34 +0100

    Add stub class definition

---
+++ src/jiraPlusPlus/JiraElectronicBoard.java
@@ -0,0 +1,11 @@
+package jiraPlusPlus;
+
+import java.util.List;
+
+public class JiraElectronicBoard implements IElectronicBoard {
+    public void Populate(List<List<Ticket>> tickets) {
+    }
+
+    public void Sync() {
+    }
+}
---
+++ src/jiraPlusPlus/Ticket.java
@@ -0,0 +1,13 @@
+package jiraPlusPlus;
+
+public class Ticket {
+    private int id;
+
+    public Ticket(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+}
